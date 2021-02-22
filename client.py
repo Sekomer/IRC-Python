a1154fe017b42f5d3862f7c9b312049aebe9a435
Alperen Serkan Aksöz <61659936+Sekomer@users.noreply.github.com>
2021-02-23 00:47:25 +0300

    added main to client

--- client.py
+++ client.py
@@ -1,25 +1,36 @@
 import socket
-from itertools import count
+import time
+import os
 #from socket_utils import *
 
 HEADER_SIZE = 4
 
 USER_NAME = input("Username: ")
 
-IP = "127.0.0.1"
+IP = socket.gethostbyname(socket.gethostname())
 PORT = 4242
+
 ENCODING = "utf-8"
 
 host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host.connect((socket.gethostname(), PORT))
-
-
 #sock.setblocking(False)
 
 
-for i in count(0):
-    message = input(f"{USER_NAME}$ ")
-    message = message.encode(ENCODING)
-    HEADER = f"{ len(message) : <{HEADER_SIZE}}"
-    PACKET = HEADER.encode(ENCODING) + message 
-    host.send(PACKET)
+def main():
+    for i in range(1, 4):
+        print("Connecting" + i * '.'); time.sleep(i)
+        os.system('cls')
+
+    print(f"Hello {USER_NAME}! Welcome to IRC, press Q if you would like to exit.", end='\n\n')
+
+    while True:
+        message = input(f"{USER_NAME}$ ")
+        message = message.encode(ENCODING)
+        HEADER = f"{ len(message) : <{HEADER_SIZE}}"
+        PACKET = HEADER.encode(ENCODING) + message 
+        host.send(PACKET)
+
+
+if __name__ == '__main__':
+    main()
