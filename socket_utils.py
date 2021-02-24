92661fd865b9bd6d298d480322321dbf1c3d4ae0
Alperen Serkan Aksöz <61659936+Sekomer@users.noreply.github.com>
2021-02-24 17:28:04 +0300

    util

--- socket_utils.py
+++ socket_utils.py
@@ -1,8 +1,70 @@
 from os import system
 from time import sleep
+import socket
 import threading
 
 
+#class begin#
+class mySocket(object):
+    
+    def __init__(self 
+                ,sock
+                ,name 
+                ,ip
+                ,port         = 4242
+                ,family       = socket.AF_INET
+                ,protocol     = socket.SOCK_STREAM
+                ,encoding     = 'utf-8'
+                ,headerSize   = 4 
+                ,isServer     = False
+    ):                         
+        self._SOCK            = sock
+        self.name             = name
+        self.ip               = ip 
+        self.port             = port
+        self.family           = family
+        self.protocol         = protocol
+        self._ENCODING        = encoding
+        self._HEADER_SIZE     = headerSize
+        self.isServer         = isServer
+        self.receivedMessages = 0                  # Number of messages
+        self.sentBytes        = 0
+        self.receivedBytes    = 0                  # Without header!
+        self.numClients       = 0
+        self.clients          = dict()
+
+    def initialize(self):
+        self._SOCK = socket.socket(self.family, self.protocol)
+        self._SOCK.bind((self.ip, self.port))
+        print("Server initialized"
+             ,f"[IP] {self.ip}"
+             ,f"[Port] {self.port}"
+             ,sep = "\n")
+        
+    def listen(self, num):    
+        self._SOCK.listen(num)
+
+    def accept(self):
+        self.numClients += 1
+        client, clientAddr = self._SOCK.accept()
+        self.clients[clientAddr[0]] = client      # ip adress => key, client socket => value
+        return (client, clientAddr)
+
+    def broadcast(self, message):
+        pass
+    
+    def receive(self, clientSocket):
+        packetSizeRaw = clientSocket.recv(self._HEADER_SIZE)
+        packetSizeString = packetSizeRaw.decode(encoding=self._ENCODING).split()[0]
+        packetSize = int(packetSizeString, 10)
+        
+        messageRaw = clientSocket.recv(packetSize)
+        message = messageRaw.decode(encoding=self._ENCODING)
+        
+        self.receivedMessages += 1
+        self.receivedBytes += packetSize
+        return message
+#class end#
 
 class safeThread(threading.Thread):
     def __init__(self, *args, **kwargs):
@@ -36,14 +98,12 @@ def packetProcess(USER_NAME, HEADER_SIZE, ENCODING):
     PACKET = HEADER.encode(ENCODING) + message 
     return PACKET
 
-def clearScreen():
-    ret = system('clear')
-    # ret = 0 ==> success
-    # ret = 1 ==> failure
-    if ret:    
+def clearScreen(platform):
+    if platform == 'Windows':
         system('cls')
+    else:
+        system('clear')
 
-#brutal fantasia
 def connection():
     for i in range(1, 4):
         print("Connecting" + i * '.') 
